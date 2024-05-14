46a93427d75370ad7c583f1a542f812dabf378a6
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-14 19:38:56 +0530

    Add Initial file

---
+++ BitManipulation.java
@@ -0,0 +1,10 @@
+public class BitManipulation {
+    public static void main(String[] args) {
+        System.out.println(5 & 6); // AND Operator
+        System.out.println(5 | 6); // OR Operator
+        System.out.println(5 ^ 6);// XOR Operator
+        System.out.println(~5);// ONE's Complement
+        System.out.println(5 << 2);// Left Shift
+        System.out.println(6 >> 2); // Right Shift
+    }
+}
