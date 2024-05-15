ee8fe505fe1af06b4aae5f75ff60230193156cb0
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-15 21:00:13 +0530

    add files

---
+++ ClearIthBit.java
@@ -0,0 +1,11 @@
+public class ClearIthBit {
+    public static int clearUthbit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n | bitMask;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        int i = 2;
+        System.out.println(clearUthbit(n, i));
+    }
+}
