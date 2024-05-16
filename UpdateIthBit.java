29e9906d6254846213afd451038b67fd341bc7ba
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-16 20:30:35 +0530

    Modify

--- UpdateIthBit.java
+++ UpdateIthBit.java
@@ -1,5 +1,26 @@
 public class UpdateIthBit {
+    public static int clearIthbit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n | bitMask;
+    }
+
+    public static int setIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        return n | bitMask;
+    }
+
+    public static int updateIthBitt(int n, int i, int newBit) {
+        // n = clearIthbit(n, i);
+        // int bitMask = newBit << i;
+        // return n | bitMask;
+        if (newBit == 0) {
+            return clearIthbit(n, i);
+        } else {
+            return setIthBit(n, i);
+        }
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(updateIthBitt(10, 2, 1));
     }
 }
